95223aabd96d9d009a96d1942728c954f7299325
ayachang <aya.chang@104.com.tw>
2020-08-13 17:46:14 +0800

    Item.class內的屬性 改成private, 並且進行封裝

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,53 +8,52 @@ class GildedRose {
     }
 
     public void updateQuality() {
-       //改成另一種for each 迴圈方式較好閱讀, 而不是使用index loop
     	for (Item item : items) {
-    		 if (!item.name.equals("Aged Brie")
-                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                 if (item.quality > 0) {
-                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                         item.quality = item.quality - 1;
+    		 if (!item.getName().equals("Aged Brie")
+                     && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                 if (item.getQuality() > 0) {
+                     if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                         item.setQuality(item.getQuality() - 1);
                      }
                  }
              } else {
-                 if (item.quality < 50) {
-                     item.quality = item.quality + 1;
+                 if (item.getQuality() < 50) {
+                	 item.setQuality(item.getQuality() + 1);
 
-                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                         if (item.sellIn < 11) {
-                             if (item.quality < 50) {
-                                 item.quality = item.quality + 1;
+                     if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                         if (item.getSellIn() < 11) {
+                             if (item.getQuality() < 50) {
+                            	 item.setQuality(item.getQuality() + 1);
                              }
                          }
 
-                         if (item.sellIn < 6) {
-                             if (item.quality < 50) {
-                                 item.quality = item.quality + 1;
+                         if (item.getSellIn() < 6) {
+                             if (item.getQuality() < 50) {
+                            	 item.setQuality(item.getQuality() + 1);
                              }
                          }
                      }
                  }
              }
 
-             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                 item.sellIn = item.sellIn - 1;
+             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                 item.setSellIn(item.getSellIn() - 1);
              }
 
-             if (item.sellIn < 0) {
-                 if (!item.name.equals("Aged Brie")) {
-                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                         if (item.quality > 0) {
-                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                 item.quality = item.quality - 1;
+             if (item.getSellIn() < 0) {
+                 if (!item.getName().equals("Aged Brie")) {
+                     if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                         if (item.getQuality() > 0) {
+                             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                                 item.setQuality(item.getQuality() - 1);
                              }
                          }
                      } else {
-                         item.quality = item.quality - item.quality;
+                         item.setQuality(item.getQuality() - item.getQuality());
                      }
                  } else {
-                     if (item.quality < 50) {
-                         item.quality = item.quality + 1;
+                     if (item.getQuality() < 50) {
+                         item.setQuality(item.getQuality() + 1);
                      }
                  }
              }
